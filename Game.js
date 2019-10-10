00375c990e74a5a1ad1f36783cdb878ad0ca8de9
Israel <keremellla@gmail.com>
2019-10-09 20:23:42 -0400

    fin

--- Game.js
+++ Game.js
@@ -4,6 +4,11 @@ const GameState = Object.freeze({
     THIRD: Symbol("THIRD"),
     FOURTH: Symbol("FOURTH"),
     FIFTH: Symbol("FIFTH"),
+    SIX: Symbol("SIX"),
+    SEVENTH: Symbol("SEVENTH"),
+    EIGHT: Symbol("EIGHT"),
+    NINETH: Symbol("NINETH"),
+    FINAL: Symbol("FINAL"),
     TOAST: Symbol("toast")
 });
 
@@ -52,14 +57,73 @@ export default class Game{
                 break;
                 case GameState.FIFTH:
                 if(sInput.toLowerCase().match("give him drink" || "give drink")){
-                    sReply = "The man thanks you and finishes the water in one gulp. Now breathing deeply he tells you to lock all doors. Ask him why or do it?";
-                    this.stateCur = GameState.SECOND;
+                    sReply = "The man thanks you and finishes the water in one gulp. Now breathing deeply he tells you to lock all doors quickly. Ask him why or do it?";
+                    this.stateCur = GameState.SIX;
 
-                }else{
+                }else if (sInput.toLowerCase().match("do it")){
+                    sReply = "Kill one save a thousand!";
+                    this.stateCur = GameState.SIX;
+                }
+                else{
                     sReply = "Kill one save a thousand!";
                     this.stateCur = GameState.FOURTH;
                 }
                 break;
+                case GameState.SIX:
+                if(sInput.toLowerCase().match("why")){
+                    sReply = "Just lock the doors! we don't have much time he shouts. he getts up and walks to the windows still dragging his foot. He tells you to look for a weapon. you look in the drawer and find a bread knife and butcher knife. choose your weapon!";
+                    this.stateCur = GameState.SEVENTH;
+
+                }else if ("do it" || "lock" || "lock doors"){
+                    sReply = "You lock all the doors and he tells you that he was bit by a wolf while he was out walking in the woods. Ask him to leave so he doesnt turn into a wearwolf or let him stay?";
+                    this.stateCur = GameState.SEVENTH;
+                }
+
+                else{
+                      sReply = "You shall not enter....if you lock!";
+                      this.stateCur = GameState.FIFTH;
+
+                }
+                break;
+                case GameState.SEVENTH:
+                    if(sInput.toLowerCase().match("bread" || "bread knife")){
+                        sReply = "He shouts thats not going to work get something else. You hear you windows breaking";
+                        this.stateCur = GameState.SIX;
+                    }
+                    else if(sInput.toLowerCase().match("butcher" || "butcher knife")){
+                        sReply = "Pass it quickly he shouts there is something at the door!, you give it to him then he runs to the door and asks you to open it slowly while he's staying on the side. Do it and chicken out!"
+                        this.stateCur = GameState.EIGHT;
+                    }
+                    else{
+                        sReply = "Do your best to stay alive";
+                        this.stateCur = GameState.SIX;
+                    }
+                case GameState.EIGHT:
+                    if(sInput.toLowerCase().match("do it" || "open it")){
+                        sReply = "As you open the door slowly a wearwolf jumps thru the door and trys to bite your neck. Ask for the knife or use ur hands to fight the wolf ";
+                    }
+                    else{
+                        sReply = "The man treatens to kill you if you don't open the door."
+                        this.stateCur = GameState.SEVENTH;
+                    }
+                case GameState.NINETH:
+                    if (sInput.toLowerCase().match("ask" || "knife" || "ask for the knife" || "ask knife" || "the knife")){
+                        sReply = "He watches while you ask for the knife and laughs. His eyes are turning red and you could see his face turning hairy. Finish what you started or give up.";
+                        this.stateCur = GameState.FINAL;
+                    }
+                    else if (sInput.toLowerCase().match("fight" || "fight the wolf")){
+                        sReply = "You manage to fight the wolf but you died of your injuries."
+                        this.stateCur = GameState.EIGHT;
+                    }
+                case GameState.FINAL:
+                    if (sInput.toLowerCase().match("finish" || "finish it")){
+                        sReply = "You break the jaw of the wolf and got up. The man howls and attacks but you evade him and kill him with his own knife that he was holding. You decided never to let any sketchy people in your house ever. FIN "
+                        this.stateCur = GameState.FIRST;
+                    }
+                    else if(sInput.toLowerCase().match("give up" || "giveup")){
+                        sReply = "The wolf kills you and drags your body outside. The wolf turns into a women and goes into the house to live with the man happ forever in your house. Lesson learned. FIN"
+                        this.stateCur = GameState.FIRST;
+                    }
         }
         return([sReply]);
     }
